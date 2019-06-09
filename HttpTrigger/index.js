49d459432aefa20db51062e7dc4066568e0d0615
planethouki <26595148+planethouki@users.noreply.github.com>
2019-06-09 10:45:26 +0900

    dragon api

--- HttpTrigger/index.js
+++ HttpTrigger/index.js
@@ -1,16 +1,57 @@
+const {
+    Account,
+    Address,
+    Deadline,
+    Mosaic,
+    MosaicId,
+    NetworkType,
+    PlainMessage,
+    TransactionHttp,
+    TransferTransaction,
+    UInt64
+} = require('nem2-sdk');
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
+    const privateKey = process.env.PRIVATE_KEY;
+    const networkType = NetworkType[process.env.NETWORK_TYPE];
+    const endpoint = process.env.ENDPOINT;
+    const generationHash = process.env.GENERATION_HASH;
+    const mosaicId = new MosaicId(process.env.MOSAIC_ID);
+    const amount = UInt64.fromUint(process.env.AMOUNT);
+    const fee = UInt64.fromUint(process.env.FEE);
+
+    const account = Account.createFromPrivateKey(privateKey, networkType);
+
+    if (req.query.recipient || (req.body && req.body.recipient)) {
+        const recipient = Address.createFromRawAddress(req.query.recipient || req.body.recipient);
+        const transactionHttp = new TransactionHttp(endpoint);
+        const tx = TransferTransaction.create(
+            Deadline.create(),
+            recipient,
+            [new Mosaic(mosaicId, amount)],
+            PlainMessage.create(''),
+            networkType,
+            fee
+        );
+        const signedTx = account.sign(tx, generationHash);
+        await transactionHttp.announce(signedTx).toPromise().then(() => {
+            context.res = {
+                // status: 200, /* Defaults to 200 */
+                body: signedTx.hash
+            };
+        }).catch(() => {
+            context.res = {
+                status: 500,
+                body: "Transaction annoucne fail"
+            };
+        })
     }
     else {
         context.res = {
             status: 400,
-            body: "Please pass a name on the query string or in the request body"
+            body: "Please pass a recipient on the query string or in the request body"
         };
     }
 };
